fa9aa47eb87702a374275781db602cec45fcbdd8
Addo <developer@addoeventi.it>
2020-11-06 13:32:32 +0100

    edit install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ sudo apt install git
 sudo git clone https://github.com/addoeventi/addo.tech-release.git /home/addo/
 sudo npm i -g pm2
 sudo pm2 start /home/addo/addo-totem/server/index.js name="Addo Totem API"
+sudo pm2 save
+sudo pm2 startup
+sudo chmod +x /home/addo/addo-totem/start.sh
+sudo chmod +x /home/addo/addo-totem/startup.sh
 sudo cp /home/addo/addo-totem/start.sh.desktop ~/.config/autostart
 sudo cp /home/addo/addo-totem/startup.sh.desktop ~/.config/autostart
 sudo reboot
