26c2716d60e95c85360195e421f4853cced87c6e
addoeventi <46922110+addoeventi@users.noreply.github.com>
2021-08-02 19:05:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ sudo apt install curl unclutter openssh-server -y
 sudo systemctl status ssh
 sudo ufw allow ssh
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt install npm
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt install nodejs git git-lfs -y
 sudo git lfs clone https://github.com/addoeventi/addo.tech-release.git /home/addo/addo-totem
@@ -14,8 +15,6 @@ sudo pm2 startup
 sudo pm2 save
 sudo chmod +x /home/addo/addo-totem/start.sh
 sudo chmod +x /home/addo/addo-totem/startup.sh
-sudo chmod +x /home/addo/addo-totem/update-scripts/pre-update.sh
-sudo chmod +x /home/addo/addo-totem/update-scripts/post-update.sh
 sudo mkdir ~/.config/autostart
 sudo cp /home/addo/addo-totem/start.sh.desktop ~/.config/autostart
 sudo cp /home/addo/addo-totem/startup.sh.desktop ~/.config/autostart
